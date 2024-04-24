177cdd3410535d8fdbbbc5cdfb104a1d35b1aaa1
Nisanth Yaganti <yagantinisanth@gmail.com>
2024-04-24 18:36:35 -0400

    login with account

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Accounts
+# module Accounts
   # An individual or service, uniquely identified by provider and uid, authenticated by a trusted
   # id_token2 Service Provider
   class Identity
@@ -29,19 +29,48 @@ module Accounts
       { provider: id_token[:iss], uid: id_token { :sub } }
     end
 
-    def self.find_or_initialize_with_id_token(id_token: nil)
-      find_by_id_token(id_token: id_token) || initialize_with_id_token(id_token: id_token)
+    def self.fetch_with_oidc(auth)
+      find_by_id_token(auth) || persist_by_id_token(auth)
     end
 
-    def self.find_by_id_token(id_token: nil)
-      id_token.symbolize_keys!
-      account = Accounts::Account.where('identities.uid': id_token[:uid], 'identities.provider': id_token[:provider]).first
-      account&.identities&.where(uid: id_token[:uid], provider: id_token[:provider])&.first
+    def self.find_by_id_token(auth)
+      account = Account.where(:'identities.provider' => auth.provider, :'identities.uid' => auth.uid).first
+      return if account.nil?
+
+      account.identities.where(provider: auth.provider, uid: auth.uid).first
     end
 
-    def self.initialize_with_id_token(id_token: nil)
-      id_token.symbolize_keys!
-      new(uid: id_token[:uid], provider: id_token[:provider])
+    def self.persist_by_id_token(auth)
+      info = auth.info
+      account = Account.find_or_initialize_by(name: info.name)
+      unless account.persisted?
+        account.confirmed_at = Time.now.utc
+        account.email = info.email
+      end
+      identity = account.identities.build(
+        provider: auth.provider,
+        uid: auth.ui,
+        email: info.email,
+        first_name: info.first_name,
+        last_name: info.last_name
+      )
+      account.save
+      identity
     end
+
+    # def self.find_or_initialize_with_id_token(id_token: nil)
+    #   find_by_id_token(id_token: id_token) || initialize_with_id_token(id_token: id_token)
+    # end
+
+    # def self.find_by_id_token(id_token: nil)
+    #   id_token.symbolize_keys!
+    #   account = Accounts::Account.where('identities.uid': id_token[:uid], 'identities.provider': id_token[:provider]).first
+    #   account&.identities&.where(uid: id_token[:uid], provider: id_token[:provider])&.first
+    # end
+
+    # def self.initialize_with_id_token(id_token: nil)
+    #   id_token.symbolize_keys!
+    #   new(uid: id_token[:uid], provider: id_token[:provider])
+    # end
   end
-end
+# end
